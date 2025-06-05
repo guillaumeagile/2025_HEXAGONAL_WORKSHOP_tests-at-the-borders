4a35a5a96075890d19c7be8e803d386ba19a2e07
guillaume prof <guillaume.saintetienne@iter.org>
2025-06-05 22:55:27 +0200

    version avec MockK (ca ne marchait car pb de bande passante sur site, pas pu récuper les jars complemtement)
    anyway, les mocks, c'est vraiment pas fiable, la preuve:
    
    le setup est compliqué (et ici ca fait planter le test)
    et meme si on le règle, on peut juste s'en servir de SPY mais pas comme une vraie doublure fiable

--- src/test/kotlin/atelier2025/useCases/antiseche/PaiementLocationTests.kt
+++ src/test/kotlin/atelier2025/useCases/antiseche/PaiementLocationTests.kt
@@ -4,12 +4,15 @@ import adapters.autres_adapters_fakes.FausseHorloge
 import atelier2025.adapters.driven.storage.InMemoryStockage
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import io.mockk.*
 import kotlinx.datetime.LocalDateTime
+import location.domain.entities.Ticket
 import location.domain.useCases.PaiementLocation
 import location.domain.usine.UsineDeTickets
 import location.domain.usine.regles.CalculPrixHauteSaison
 import location.domain.valueObjects.DureeDeLocation
 import location.ports.PourAvoirHeure
+import location.ports.PourStockage
 import location.utilities.TestableIdGenerateur
 
 
@@ -21,29 +24,46 @@ class PaiementLocationTests: BehaviorSpec( {
        // val adapter =   XAdapter() as PourStockage  // à vous de jouer:
         // écrivez le port et l'adapter qui va bien pour notre métier
         val doublurePourStockage = InMemoryStockage()
+        //ou bien un mock?
+        val mocked = mockk<PourStockage>(relaxed = true)
+        every { mocked.StockerTicket(any<Result<Ticket>>()) } returns  Result.success(Ticket.enEchec())
+        // les ennuis commencent:
+        // Failed matching mocking signature for
+        //io.mockk.MockKException: Failed matching mocking signature for
+        //SignedCall(retValue=, isRetValueMock=true, retType=class kotlin.Any, self=PourStockage(#1), method=StockerTicket-bjn95JY(Any), args=[java.lang.Object@7b61a6b5], invocationStr=PourStockage(#1).StockerTicket-bjn95JY(java.lang.Object@7b61a6b5))
+        //left matchers: [any()]
 
         val usineDeTickets = UsineDeTickets(TestableIdGenerateur(), CalculPrixHauteSaison())
-        // on peut aussi utiliser RegleDePrixPourTests
 
-        val aTester = PaiementLocation(usineDeTickets, doublurePourStockage, fausseHorloge)
+
+        val aTester = PaiementLocation(usineDeTickets, mocked, fausseHorloge)
 
         When("je loue pour 15 minutes") {
 
-            doublurePourStockage.reset()
+            //doublurePourStockage.reset()
             val actualTicket = aTester.payerLocationImmediate (DureeDeLocation(15))
 
 
             Then("le ticket est enregistré") {
                 actualTicket.isSuccess shouldBe true
-                doublurePourStockage.length shouldBe 1
+                verify(exactly = 1) { mocked.StockerTicket(any<Result<Ticket>>()) }
+
+                // on peut juste verifier les appels, mais ensuite si on doit mock la relecture
+                // c'est trop facile de créer des états incohérents
+                // on est incapable de faire des assertions fiables et réalistes sur le stockage
+
+                //d'où l'avantage de la doublure (fake) en mémoire par exemple, dont le comportement va vraiment
+                // imiter une base de données, dans differents scénarios (mais il convient de la tester aussi)
+
+                // doublurePourStockage.length shouldBe 1
             }
         }
 
         When("je fais rien"){
-            doublurePourStockage.reset()
+           // doublurePourStockage.reset()
 
             Then("le ticket n'est pas enregistré") {
-                doublurePourStockage.length shouldBe 0
+             //   doublurePourStockage.length shouldBe 0
             }
         }
 
