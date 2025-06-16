f2a1112c748e86e2e131463b83f866a273d476b1
guillaume prof <guillaume.saintetienne@iter.org>
2025-06-16 18:48:46 +0200

    wip

--- src/test/kotlin/atelier2025/domain/RegleDePrixAvecFidelitéTests.kt
+++ src/test/kotlin/atelier2025/domain/RegleDePrixAvecFidelitéTests.kt
@@ -36,5 +36,25 @@ class RegleDePrixAvecFidelitéTests : FeatureSpec({
             sut.QuelleRemiseAppliquer shouldBe Remise.Totale
         }
 
+        scenario("si dans les 4 derniers locations, une a bénéficié de la remise totale, la 5e n'est pas gratuite")
+        {
+            val stockage = StubLireLocation() as PourLireLesLocations
+            val location1 = Location( "1", "Alice", Remise.Totale)
+            val location2 = Location ( "2", "Alice", Remise.Aucune)
+            val location3 = Location( "3", "Alice", Remise.Aucune)
+            val location4 = Location( "4", "Alice", Remise.Aucune)
+
+            stockage.enregistrer(location1)
+            stockage.enregistrer(location2)
+            stockage.enregistrer(location3)
+            stockage.enregistrer(location4)
+
+            // setMock  pour que  retourne  NombreDeLocations = 4
+            val sut = RemiseAvecProgrammeFidelité(stockage)
+
+            sut.QuelleRemiseAppliquer shouldBe Remise.Aucune
+        }
+
+
     }
 })
