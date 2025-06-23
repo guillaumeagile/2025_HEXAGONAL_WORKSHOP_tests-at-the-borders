0dd6c900cc3543d234c39f1d31be0ae21a1b6d01
acassaigne <anthony.cassaigne@gmail.com>
2025-06-23 17:01:09 +0200

    Version 2 of getQuelleRemiseAppliquer2 fix the bug.

--- src/test/kotlin/sunny2025/domain/RemiseAvecProgrammeFidelité.kt
+++ src/test/kotlin/sunny2025/domain/RemiseAvecProgrammeFidelité.kt
@@ -6,17 +6,22 @@ import location.abstractions.PourLireLesLocations
 class RemiseAvecProgrammeFidelité(val stockage: PourLireLesLocations) : PourDeterminerUneRemiseCommerciale {
 
 
-    fun getQuelleRemiseAppliquer(client : String = "Alice"): Remise =
+    fun getQuelleRemiseAppliquer(client: String = "Alice"): Remise =
         when {
-            stockage.nombreDeLocations( client) == 0 -> Remise.Aucune
-            stockage.donneMoiLes4DernieresLocations( client).any() { it.remise == Remise.Totale } -> Remise.Aucune
+            stockage.nombreDeLocations(client) == 0 -> Remise.Aucune
+            stockage.donneMoiLes4DernieresLocations(client).any() { it.remise == Remise.Totale } -> Remise.Aucune
             else -> Remise.Totale
         }
 
+    companion object {
+        const val SEUIL_REMISE_TOTAL = 4
+    }
 
-    fun getQuelleRemiseAppliquer2(client : String = "Alice"): Remise =
-       if (stockage.donneMoiLes4DernieresLocations( client).all() { it.remise == Remise.Aucune })
-            Remise.Totale
-        else Remise.Aucune
-
+    fun getQuelleRemiseAppliquer2(client: String = "Alice"): Remise {
+        if (stockage.nombreDeLocations(client) == SEUIL_REMISE_TOTAL &&
+            stockage.donneMoiLes4DernieresLocations(client).all({ it.remise == Remise.Aucune })
+        )
+            return Remise.Totale
+        return Remise.Aucune
+    }
 }
