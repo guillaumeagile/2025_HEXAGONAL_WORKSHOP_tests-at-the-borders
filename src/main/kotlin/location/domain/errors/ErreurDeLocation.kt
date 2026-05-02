9c6fcd7b872f2a5ea4298dbf9aba1f2d1a92a19c
GuillaumeToulouse <yohm31@gmail.com>
2026-05-02 16:49:48 +0200

    empty old file

--- src/main/kotlin/location/domain/errors/ErreurDeLocation.kt
+++
@@ -1,8 +0,0 @@
-package location.domain.errors
-
-sealed interface ErreurDeLocation {
-    data object DureeInvalide : ErreurDeLocation
-    data object IdManquant : ErreurDeLocation
-    data object PrixInvalide : ErreurDeLocation
-    data class TicketIntrouvable(val id: String) : ErreurDeLocation
-}
